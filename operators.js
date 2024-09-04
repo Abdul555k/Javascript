6fae35be0672ba2cdfa51d4599ee093f133fab23
Abdul555k <abdullahkhalil11120@gmail.com>
2024-09-04 15:53:15 +0500

    feat:operators tutorial

---
+++ operators.js
@@ -0,0 +1,34 @@
+let num1 = 12;
+let num2 = 5;
+
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let remainder = num1 % num2;
+let exponentiation = num1 ** num2;
+
+document.write("Addition: " + addition);            // 17
+
+document.write('<br>')
+
+document.write("Subtraction: " + subtraction);      // 7
+
+document.write('<br>')
+
+document.write("Multiplication: " + multiplication);// 60
+
+document.write('<br>')
+
+document.write("Division: " + division);            // 2.4
+
+document.write('<br>')
+
+document.write("Remainder: " + remainder);          // 2
+
+document.write('<br>')
+
+document.write("Exponentiation: " + exponentiation); // 248832
+
+document.write('<br>')
